d1bc561af9d6cbe3b991e208b632a9367a94077c
apoorvedave <a1dave@ucsd.edu>
2016-03-14 23:41:26 -0700

    102. Level Order Traversal

---
+++ 102/Solution.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.Queue;
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root==null){
+            return result;
+        }
+        
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        
+        // base
+        q1.add(root);
+        List<Integer> l = new ArrayList<>();
+        l.add(root.val);
+        result.add(l);
+        
+        //create alternatively lists using queues and add to result
+        while(!q1.isEmpty() || !q2.isEmpty()){
+            Queue<TreeNode> full = (q1.isEmpty())? q2:q1;
+            Queue<TreeNode> empty = (q1.isEmpty())? q1:q2;
+            List<Integer> l2 = new ArrayList<>();
+            while(!full.isEmpty()){
+                // transfer kids of full to empty,
+                // add kids to l2
+                TreeNode p = full.poll();
+                if (p.left!=null) {
+                    l2.add(p.left.val);
+                    empty.add(p.left);
+                }
+                if (p.right!=null) {
+                    l2.add(p.right.val);
+                    empty.add(p.right);
+                }
+            }
+            if (!l2.isEmpty()){
+                result.add(l2);
+            }
+        }
+        return result;
+    }
+}
